3fbb54d36a224f7a3a93da848f64882b07b7a134
kyle10n <kyle.a.tenn@gmail.com>
2025-08-19 19:05:29 +0200

    Initialized new repo, with some instructions and the example files.

---
+++ README.md
@@ -0,0 +1,8 @@
+python PIC.py ex.treefile ex.fa
+
+
+For dependencies
+>>pip install biopython weblogo matplotlib numpy
+
+If you are on mac
+>>brew install ghostscript
---
+++ code/config.py
@@ -0,0 +1,12 @@
+updated_dict = None
+seq_dict = None 
+seq_counter = None 
+
+matrix = None
+charaters = set()
+available_characters = None
+existing_characters = None
+
+seq_type = 'dna'
+seq_length = 0
+terminals = 0
